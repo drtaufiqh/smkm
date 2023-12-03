bb40a671b7ab67d6346af1896d47c8fa9e516d7b
DUTATAMA ROSEWIKA TAUFIQ HADIHARDAYA <222111997@stis.ac.id>
2023-12-03 13:07:58 +0700

    fix ubah password mahasiswa

--- app/Http/Controllers/RoleMahasiswaController.php
+++ app/Http/Controllers/RoleMahasiswaController.php
@@ -89,11 +89,36 @@ class RoleMahasiswaController extends Controller
 
     public function ubah_password(Request $request, $id_user)
     {
+        $user = User::find($id_user);
+        $email = $request->input('email');
+        $password_lama = $request->input('password_lama');
+        
+        // if(bcrypt($request->input('password_lama')) != $user->password){
+        //     return redirect()->to('/mahasiswa/password')->with('failed', 'Password lama salah');
+        // }
+
+        // if (Auth::user()->getAuthPassword() != bcrypt($request->input('password_lama'))) {
+        //     // Password benar
+        // } 
+
+        if (!Auth::attempt(['email' => $email, 'password' => $password_lama])) {
+            return redirect()->to('/mahasiswa/password')->with('failed', 'Password lama salah');
+        }
+
+        $request->validate([
+            'password_baru'=>['required'],
+            'ulangi_password_baru'=>['required','same:password_baru']
+        ], [
+            'password_baru.required' => "Masukkan Password Baru",
+            'ulangi_password_baru.required' => "Masukkan Konfirmasi Password",
+            'ulangi_password_baru.same' => "Konfirmasi Password tidak sama dengan Password baru"
+        ]);
+
         $data = [
-            'password' => $request->input('password_baru')
+            'password' => bcrypt($request->input('password_baru'))
         ];
 
-        User::where('id',$id_user)->update($data);
+        $user->update($data);
         return redirect()->to('/mahasiswa/password')->with('success', 'Berhasil mengubah password');
     }
 
--- resources/views/Mahasiswa/password.blade.php
+++ resources/views/Mahasiswa/password.blade.php
@@ -40,22 +40,22 @@
                   {{-- <h5 class="card-title">Akun</h5> --}}
                     @include('komponen.pesan')
 
+                    <form method="POST" action="{{ route('ubah_password_mahasiswa', ['id' => Auth::user()->id]) }}" enctype="multipart/form-data">
+                      @csrf
+                      @method('PUT')
                     <div class="row mt-2 mb-3 disabled">
                       <label for="email" class="col-md-4 col-lg-3 col-form-label">Email</label>
                       <div class="col-md-8 col-lg-9">
-                        <input  name="email" class="form-control" id="email" value="{{ Auth::user()->email }}" >
+                        <input  name="email" class="form-control" id="email" value="{{ Auth::user()->email }}" readonly>
                       </div>
                     </div>
 
                     <div class="row mb-3">
-                      <label for="password" class="col-md-4 col-lg-3 col-form-label">Password Lama</label>
+                      <label for="password_lama" class="col-md-4 col-lg-3 col-form-label">Password Lama</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="password" type="password" class="form-control" id="password" value="{{ Auth::user()->password }}">
+                        <input name="password_lama" type="password" class="form-control" id="password_lama">
                       </div>
                     </div>
-                  <form action="" method="POST" action="{{ route('ubah_password_mahasiswa', ['id' => Auth::user()->info()->id]) }}" enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
 
                     <div class="row mb-3">
                       <label for="password_baru" class="col-md-4 col-lg-3 col-form-label">Password Baru</label>
@@ -67,7 +67,7 @@
                     <div class="row mb-3">
                       <label for="ulangi_password_baru" class="col-md-4 col-lg-3 col-form-label">Ulangi Password Baru</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="ulangi_password_baru" class="form-control" id="ulangi_password_baru">
+                        <input name="ulangi_password_baru" type="password" class="form-control" id="ulangi_password_baru">
                       </div>
                     </div>
   
